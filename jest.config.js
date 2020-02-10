9c230b35e8c5dee83b4015df016c8ff1ca978dd3
felberto <tobias.felber97@gmail.com>
2020-02-10 13:13:37 +0100

    fix jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-    globals: {
-        'ts-jest': {
-            diagnostics: false,
-        },
-    },
     preset: "ts-jest",
     collectCoverage: true,
     collectCoverageFrom: ["src/{renderer}/**/*.{js,jsx,ts,tsx}"],
