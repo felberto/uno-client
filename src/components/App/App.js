d0fd7c0a723e5b502dd14ecbba104bb4c9eb0442
felberto <tobias.felber97@gmail.com>
2020-03-01 14:38:55 +0100

    added modals

---
+++ src/components/App/App.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import logo from '../../logo.svg';
+import socketIOClient from "socket.io-client";
+import {Button} from "react-bootstrap";
+import CreateLobbyModal from '../Modal/CreateLobbyModal';
+import JoinLobbyModal from "../Modal/JoinLobbyModal";
+
+class App extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            endpoint: 'http://localhost:8001/',
+            show: false
+        };
+        let socket = socketIOClient(this.state.endpoint);
+    }
+
+    render() {
+        let createLobbyModalClose = () => this.setState({createLobbyModalShow: false});
+        let joinLobbyModalClose = () => this.setState({joinLobbyModalShow: false});
+
+        return (
+            <div>
+                <div style={{width: '100vh', height: '80vh', margin: '0 auto', padding: '10%'}}>
+                    <div style={{
+                        display: 'inline-block',
+                        float: 'left',
+                        width: '50%',
+                        height: '100%',
+                        backgroundColor: 'red'
+                    }}>
+                        <img src={logo} className="App-logo" alt="logo"/>
+                    </div>
+                    <div style={{
+                        display: 'inline-block',
+                        margin: '0 auto',
+                        padding: '3px',
+                        float: 'right',
+                        width: '50%',
+                        height: '100%',
+                        backgroundColor: 'blue'
+                    }}>
+                        <Button onClick={() => this.setState({createLobbyModalShow: true})} variant="secondary"
+                                size="lg"
+                                active style={{display: 'block'}}>
+                            Create Lobby
+                        </Button>
+                        <CreateLobbyModal
+                            show={this.state.createLobbyModalShow}
+                            onHide={createLobbyModalClose}
+                        />
+                        < br/>
+                        <Button onClick={() => this.setState({joinLobbyModalShow: true})} variant="secondary" size="lg"
+                                active
+                                style={{display: 'block'}}>
+                            Join Lobby
+                        </Button>
+                        <JoinLobbyModal
+                            show={this.state.joinLobbyModalShow}
+                            onHide={joinLobbyModalClose}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../App";
+import App from "../components/App/App";
 
 jest.mock("react-dom", () => ({render: jest.fn()}));
 
