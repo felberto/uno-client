0ee096ba447db2e11172dd02dc760fa5d9ec4293
Melanie Ockenfels <melanie.ockenfels@students.ffhs.ch>
2020-03-09 10:34:02 +0100

    leave room

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,30 @@
 import React, {Component} from "react";
+import socketInstance from "../../util/Socket";
+import {Button} from "react-bootstrap";
 
 class Game extends Component {
 
+    constructor(props) {
+        super(props);
+        this.leaveLobby = this.leaveLobby.bind(this);
+    }
+
+    leaveLobby() {
+        if (socketInstance.socket.emit('leaveRoom')) {
+            console.log('client: left game');
+            this.props.history.push('/');
+        } else {
+            console.log('could not leave room');
+            return false;
+        }
+    }
+
     render() {
-        return <div></div>
+        return (
+            <div>
+                <Button onClick={this.leaveLobby}>Lobby verlassen</Button>
+            </div>
+        )
     }
 }
 
