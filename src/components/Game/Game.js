ee1e03743cc32cd90599772c977d725e6f8e07d9
felberto <tobias.felber97@gmail.com>
2020-03-17 13:15:56 +0100

    get all room data

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,42 @@
 import React, {Component} from "react";
+import socketInstance from "../../util/Socket";
+import Button from "react-bootstrap/Button";
 
 class Game extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            roomData: {}
+        };
+        this.startGame = this.startGame.bind(this);
+    }
+
+    componentDidMount() {
+        socketInstance.socket.emit('getRoomData');
+        socketInstance.socket.on('roomData', (data) => {
+            this.setState({
+                roomData: data
+            });
+            console.log(data);
+        });
+    }
+
+    startGame() {
+        socketInstance.socket.emit('startGame', this.state.roomData.name);
+        socketInstance.socket.on('roomData', (data) => {
+            this.setState({
+                roomData: data
+            });
+        });
+    }
+
     render() {
-        return <div></div>
+        return (
+            <div>
+                <Button onClick={this.startGame}>Start</Button>
+            </div>
+        )
     }
 }
 
