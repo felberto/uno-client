ba3d32d85a6d30370dbe3dd0baa88d988431eb5d
Melanie Ockenfels <melanie.ockenfels@students.ffhs.ch>
2020-04-12 16:24:15 +0200

    Test to enter data in modal

--- src/tests/CreateLobbyModal.test.js
+++ src/tests/CreateLobbyModal.test.js
@@ -31,3 +31,25 @@ describe('test modal ', () => {
         expect(wrapper.find('button')).toHaveLength(2);
     });
 });
+
+describe('enter data', () => {
+    it('enter room name to input field', () => {
+        const findInput = wrapper => wrapper.find({'name': 'roomName'}).hostNodes();
+
+        expect(findInput(wrapper).prop('value')).toBe('');
+
+        findInput(wrapper).instance().value = 'testRoom';
+        findInput(wrapper).simulate('change');
+        expect(findInput(wrapper).prop('value')).toBe('testRoom');
+    });
+
+    it('enter name to input field', () => {
+        const findInput = wrapper => wrapper.find({'name': 'userName'}).hostNodes();
+
+        expect(findInput(wrapper).prop('value')).toBe('');
+
+        findInput(wrapper).instance().value = "testName";
+        findInput(wrapper).simulate('change');
+        expect(findInput(wrapper).prop('value')).toBe('testName');
+    });
+});
--- src/tests/JoinLobbyModal.test.js
+++ src/tests/JoinLobbyModal.test.js
@@ -34,3 +34,15 @@ describe('verify modal components', () => {
         expect(wrapper.find('button')).toHaveLength(2);
     });
 });
+
+describe('enter data', () => {
+    it('enter name to input field', () => {
+        const findInput = wrapper => wrapper.find({'name': 'userName'}).hostNodes();
+
+        expect(findInput(wrapper).prop('value')).toBe('');
+
+        findInput(wrapper).instance().value = "someName";
+        findInput(wrapper).simulate('change');
+        expect(findInput(wrapper).prop('value')).toBe('someName');
+    });
+});
